7a668d9d02c5cc01f4bb413f96ddc0afe8cfcf46
Edson Rodrigues <edsonrb.2008@gmail.com>
2026-09-09 16:24:28 -0300

    Implementação da Listagem e Cadastro de Tarefas - FASE D

--- lib/database_helper.dart
+++ lib/database_helper.dart
@@ -4,6 +4,7 @@ import 'package:sqflite/sqflite.dart';
 class DatabaseHelper {
   static Database? _db;
 
+  //Abre(ou cria, se não existe) o arquivo de banco de dados
   static Future<Database> abrirBanco() async {
     final caminho = join(await getDatabasesPath(), 'tarefas.db');
 
@@ -13,8 +14,8 @@ class DatabaseHelper {
       onCreate: (db, versao) {
         return db.execute(
           'CREATE TABLE tarefas ('
-          'id INTEGER PRIMARY KEY AUTOINCREMENT,'
-          'titulo TEXT,'
+          'id INTEGER PRIMARY KEY AUTOINCREMENT, '
+          'titulo TEXT, '
           'situacao INTEGER' //0 - False, 1 - True
           ')',
         );
@@ -22,8 +23,24 @@ class DatabaseHelper {
     );
   }
 
+  //Getter que devolve o banco de dados já aberto, ou abre se ainda não existe
   static Future<Database> get database async {
     _db ??= await abrirBanco();
     return _db!;
+  }
+
+  //READ: Buscar todas as tarefas salvas no banco
+  static Future<List<Map<String, dynamic>>> buscarTarefas() async {
+    final db = await DatabaseHelper.database;
+    return db.query('tarefas'); //SELECT * FROM tarefas
+  }
+
+  //CREATE: Inserir uma nova tarefa no banco
+  static Future<void> inserirTarefa(String titulo) async {
+    final db = await DatabaseHelper.database;
+    await db.insert('tarefas', {
+      'titulo': titulo,
+      'situacao': 0,
+    });
   }
 }
--- lib/lista_tarefa_page.dart
+++ lib/lista_tarefa_page.dart
@@ -1,55 +1,118 @@
+import 'package:app_listatarefas/database_helper.dart';
 import 'package:flutter/material.dart';
 
-class ListaTarefaPage extends StatelessWidget {
+class ListaTarefaPage extends StatefulWidget {
   const ListaTarefaPage({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    final List<Map<String, dynamic>> tarefas = [
-      {'titulo': 'Fazer Comprar', 'situacao': false},
-      {'titulo': 'Pagar conta de Luz', 'situacao': true},
-      {'titulo': 'Revisar aula de TI', 'situacao': true},
-      {'titulo': 'Pagar fatura do Inter', 'situacao': false},
-      {'titulo': 'Levar carro na manutenção', 'situacao': false},
-    ];
+  State<ListaTarefaPage> createState() => _ListaTarefaPageState();
+}
 
+class _ListaTarefaPageState extends State<ListaTarefaPage> {
+  List<Map<String, dynamic>> tarefas = [];
+
+  @override
+  void initState() {
+    super.initState();
+    carregarTarefas();
+  }
+
+  void carregarTarefas() async {
+    final dados = await DatabaseHelper.buscarTarefas();
+    setState(() {
+      tarefas = dados;
+    });
+  }
+
+  void adicionarTarefa() {
+    final novaTarefaController = TextEditingController();
+
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text('Nova Tarefa'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              TextField(
+                controller: novaTarefaController,
+                decoration: InputDecoration(
+                  hintText: 'Digite o título...',
+                ),
+              ),
+            ],
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                //Função para fechar qualquer janela/tela
+                Navigator.pop(context);
+              },
+              child: Text('Cancelar'),
+            ),
+            TextButton(
+              onPressed: () async {
+                if (novaTarefaController.text.isNotEmpty) {
+                  await DatabaseHelper.inserirTarefa(novaTarefaController.text);
+                  carregarTarefas();
+
+                  if (!context.mounted) return;
+
+                  Navigator.pop(context);
+                }
+              },
+              child: Text('Adicionar'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Minhas Tarefas"),
         centerTitle: true,
       ),
-      body: ListView.builder(
-        padding: EdgeInsets.all(12),
-        itemCount: tarefas.length,
-        itemBuilder: (context, index) {
-          final tarefa = tarefas[index];
-          final bool situacao = tarefa['situacao'];
+      body: tarefas.isEmpty
+          ? Center(
+              child: Text('Nenhuma tarefa ainda. Toque em + para adicionar'),
+            )
+          : ListView.builder(
+              padding: EdgeInsets.all(12),
+              itemCount: tarefas.length,
+              itemBuilder: (context, index) {
+                final tarefa = tarefas[index];
+                final bool situacao = tarefa['situacao'] == 1;
 
-          return Card(
-            child: ListTile(
-              leading: Icon(
-                situacao ? Icons.check_circle : Icons.circle_outlined,
-                color: situacao ? Colors.green : Colors.grey,
-              ),
-              title: Text(
-                tarefa['titulo'],
-                style: TextStyle(
-                  decoration: situacao
-                      ? TextDecoration.lineThrough
-                      : TextDecoration.none,
-                ),
-              ),
-              subtitle: Text(situacao ? 'Concluida' : 'Pendente'),
-              trailing: Icon(
-                Icons.delete_outline,
-                color: Colors.grey,
-              ),
+                return Card(
+                  child: ListTile(
+                    leading: Icon(
+                      situacao ? Icons.check_circle : Icons.circle_outlined,
+                      color: situacao ? Colors.green : Colors.grey,
+                    ),
+                    title: Text(
+                      tarefa['titulo'],
+                      style: TextStyle(
+                        decoration: situacao
+                            ? TextDecoration.lineThrough
+                            : TextDecoration.none,
+                      ),
+                    ),
+                    subtitle: Text(situacao ? 'Concluida' : 'Pendente'),
+                    trailing: Icon(
+                      Icons.delete_outline,
+                      color: Colors.grey,
+                    ),
+                  ),
+                );
+              },
             ),
-          );
-        },
-      ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () => adicionarTarefa(),
         backgroundColor: Colors.indigo,
         foregroundColor: Colors.white,
         //shape: CircleBorder(),
